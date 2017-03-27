dffb582273702431fc259a5ba7bf9e725f85f998
steelcm <csteel@gmail.com>
2017-03-27 10:50:32 +0100

    Init commit

---
+++ praw-util.py
@@ -0,0 +1,31 @@
+import time
+import requests
+import requests.auth
+import praw
+
+class PrawUtil:
+    def __init__(self, clientId, clientSecret):
+        self.clientId = clientId
+        self.clientSecret = clientSecret
+
+    def userAgent(self, username):
+        return "RedditBooks/0.1 by " + username
+
+    def getAccessToken(self, username, password):
+        client_auth = requests.auth.HTTPBasicAuth(self.clientId, self.clientSecret)
+        post_data = {"grant_type": "password", "username": username, "password": password}
+        headers = {"User-Agent": self.userAgent(username)}
+        response = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data=post_data, headers=headers)
+        return response.json()
+
+
+# todo: move this into config
+username = "terminaldevelopment"
+password = "VeAWqxRlzpoc"
+clientId = "6fr2-IRsBOxfRA"
+clientSecret = "DQqz1U_943_HTEr1dbhLzI7ZJNk"
+
+
+util = PrawUtil(clientId, clientSecret)
+print util.getAccessToken(username, password)
+
